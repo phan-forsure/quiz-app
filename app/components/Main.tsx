1b482b9b0d978b26a86971e5cbe6aecb3a13a329
phan-forsure <phantommy38@gmail.com>
2025-11-07 14:07:46 +0200

    added main screen

---
+++ app/components/Main.tsx
@@ -0,0 +1,9 @@
+import TabsComponent from "./Tabs";
+
+export default function Main() {
+  return (
+    <main className="p-12 w-[40%] h-fit bg-[#bebec1] rounded-2xl shadow-2xl text-xl text-black flex justify-center items-center">
+      <TabsComponent />
+    </main>
+  );
+}
---
+++ app/components/Tabs.tsx
@@ -0,0 +1,61 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+export default function TabsComponent() {
+  return (
+    <div className="flex w-full max-w-sm flex-col gap-6">
+      <Tabs defaultValue="account">
+        <TabsList>
+          <TabsTrigger value="account">Take a quiz</TabsTrigger>
+          <TabsTrigger value="password">Create a quiz</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">
+          <Card>
+            <CardHeader>
+              <CardTitle>Take a quiz!</CardTitle>
+            </CardHeader>
+            <CardContent className="grid gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="tabs-demo-name">Enter quiz link</Label>
+                <Input placeholder="quiz link"></Input>
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="tabs-demo-username">Join as</Label>
+                <Input placeholder="username"></Input>
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Start quiz</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+        <TabsContent value="password">
+          <Card>
+            <CardHeader>
+              <CardTitle>Create a quiz for your friends!</CardTitle>
+            </CardHeader>
+            <CardContent className="grid gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="tabs-demo-current">Quiz name</Label>
+                <Input placeholder="name"></Input>
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Start</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+}
